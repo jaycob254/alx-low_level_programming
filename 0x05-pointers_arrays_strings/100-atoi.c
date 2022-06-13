4da78b380ab50054cd8b5516227976a35e429bc0
Stanley <jaycobstano@gmail.com>
2022-06-13 04:23:11 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,43 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: the int converted from the string
  */
-void print_number(int n)
+int _atoi(char *s)
 {
-	unsigned int n1;
+	int i, d, n, len, f, digit;
 
-	if (n < 0)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+				n = n * 10 + digit;
+				f = 1;
+				if (s[i + 1] < '0' || s[i + 1] > '9')
+					break;
+				f = 0;
+		}
+		i++;
 	}
-
-	if (n1 / 10)
-		print_number(n1 / 10);
-
-	_putchar((n1 % 10) + '0');
+	if (f == 0)
+		return (0);
+	return (n);
 }
